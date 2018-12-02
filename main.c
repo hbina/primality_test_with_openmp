d94c404e341fe3d1e1becbadbeaefbca2f911534
hbina4326 <hanif.ariffin.4326@gmail.com>
2018-12-02 01:22:20 -0500

    working on graph...god

--- main.c
+++ main.c
@@ -3,9 +3,8 @@
 #include "integer_addition_up_to_n.h"
 #include "integer_primality_test.h"
 
-#define HOW_MANY_TIMES 100
-#define MAX_INTEGER_FOR_SUMMATION 100000
-#define PRIMALITY_TEST_START 1000
+#define MAX_INTEGER_IN_LOG_10 10
+#define MAX_INTEGER_IN_LOG_2 32
 #define NUM_OF_PRIME_EXAMPLES 6
 typedef struct {
     double serial_summation;
@@ -34,31 +33,42 @@ unsigned long long primes_example[6] = {
         2787253289l
 };
 
+unsigned long long ipow(unsigned long long base, unsigned char exp) {
+    for (unsigned char a = 0; a < exp; a++) {
+        base *= base;
+    }
+    return base;
+}
+
 int main() {
-    summation_statistics summation_stats[MAX_INTEGER_FOR_SUMMATION];
+    summation_statistics summation_stats[MAX_INTEGER_IN_LOG_10];
     primality_statistics primality_stats = {0, 0, 0};
 
-    /**
-    verify_summation(serial_summation, MAX_INTEGER_FOR_SUMMATION);
-    printf("serial summation tests passed\n");
-     **/
+    unsigned long long max_integer_pow10 = ipow(10l, (unsigned long long int) MAX_INTEGER_IN_LOG_10);
 
-    verify_summation(parallel_summation_using_promotion_of_scalar, MAX_INTEGER_FOR_SUMMATION);
+    verify_summation(
+            serial_summation,
+            max_integer_pow10);
+    printf("serial summation tests passed\n");
+    verify_summation(
+            parallel_summation_using_promotion_of_scalar,
+            max_integer_pow10);
     printf("parallel summation using promotion of scalar tests passed\n");
 
-    verify_summation(parallel_summation_using_atomic, MAX_INTEGER_FOR_SUMMATION);
+    verify_summation(
+            parallel_summation_using_atomic,
+            max_integer_pow10);
     printf("parallel summation using atomic tests passed\n");
 
-    verify_summation(parallel_summation_using_critical, MAX_INTEGER_FOR_SUMMATION);
+    verify_summation(
+            parallel_summation_using_critical,
+            max_integer_pow10);
     printf("parallel summation using critical tests passed\n");
-
-    /**
     verify_primality_test(
             serial_primality_test,
             primes_example,
             NUM_OF_PRIME_EXAMPLES);
     printf("serial primality test tests passed\n");
-**/
     verify_primality_test(
             parallel_primality_test_using_promotion_of_scalar,
             primes_example,
@@ -71,30 +81,31 @@ int main() {
             NUM_OF_PRIME_EXAMPLES);
     printf("parallel primality test using sentinel verification tests passed\n");
 
-    for (int integer_n = 0; integer_n < MAX_INTEGER_FOR_SUMMATION; integer_n++) {
+    for (unsigned char integer_n = 0; integer_n < MAX_INTEGER_IN_LOG_2; integer_n++) {
+        unsigned long long integer_n_pow10 = ipow(2l, integer_n);
         summation_stats[integer_n].serial_summation = addition_benchmark_wrapper(
-                &serial_summation, (unsigned long long int) integer_n);
+                &serial_summation, integer_n_pow10);
         summation_stats[integer_n].parallel_summation_using_promotion_of_scalar = addition_benchmark_wrapper(
-                &parallel_summation_using_promotion_of_scalar, (unsigned long long int) integer_n);
+                &parallel_summation_using_promotion_of_scalar, integer_n_pow10);
         summation_stats[integer_n].parallel_summation_using_atomic = addition_benchmark_wrapper(
-                &parallel_summation_using_atomic, (unsigned long long int) integer_n);
+                &parallel_summation_using_atomic, integer_n_pow10);
         summation_stats[integer_n].parallel_summation_using_critical = addition_benchmark_wrapper(
-                &parallel_summation_using_critical, (unsigned long long int) integer_n);
+                &parallel_summation_using_critical, integer_n_pow10);
     }
+
     printf("drawing\n");
     FILE *gnuplot = popen("gnuplot", "w");
     fprintf(gnuplot, "plot '-'\n");
-    for (int i = 0; i < MAX_INTEGER_FOR_SUMMATION; i++)
+    for (int i = 0; i < MAX_INTEGER_IN_LOG_2; i++)
         fprintf(gnuplot, "%d %e\n", i, summation_stats[i].parallel_summation_using_critical);
     fprintf(gnuplot, "e\n");
     fflush(gnuplot);
-    for (int a = 0; a < HOW_MANY_TIMES; a++) {
-        /**
+
+    for (int a = 0; a < 10; a++) {
         primality_stats.serial_primality_test += primality_test_benchmark_wrapper(
                 &serial_primality_test,
                 primes_example,
                 NUM_OF_PRIME_EXAMPLES);
-        **/
         primality_stats.parallel_primality_test_using_promotion_of_scalar +=
                 primality_test_benchmark_wrapper(
                         &parallel_primality_test_using_promotion_of_scalar,
@@ -107,10 +118,11 @@ int main() {
                         primes_example,
                         NUM_OF_PRIME_EXAMPLES);
     }
-    printf("serial_primality_test:\n%e\n", primality_stats.serial_primality_test / HOW_MANY_TIMES);
+
+    printf("serial_primality_test:\n%e\n", primality_stats.serial_primality_test / 10);
     printf("parallel_primality_test_using_promotion_of_scalar:\n%e\n",
-           primality_stats.parallel_primality_test_using_promotion_of_scalar / HOW_MANY_TIMES);
+           primality_stats.parallel_primality_test_using_promotion_of_scalar / 10);
     printf("parallel_primality_test_using_sentinel:\n%e\n",
-           primality_stats.parallel_primality_test_using_sentinel / HOW_MANY_TIMES);
+           primality_stats.parallel_primality_test_using_sentinel / 10);
     return 0;
 }
